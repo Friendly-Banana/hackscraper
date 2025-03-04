3ce8e1cfb8b6752671b83adccf1fcef0ac29e7c5
Gabriel <69007475+Friendly-Banana@users.noreply.github.com>
2025-03-04 01:03:28 +0100

    hackatum llm

--- hackatum.py
+++ hackatum.py
@@ -1,29 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
 import re
 
-#from general import ask_llm
+import requests
+from bs4 import BeautifulSoup
 
-url = 'https://hack.tum.de'
+url = "https://hack.tum.de"
 
-#from transformers import pipeline
-
-#qa_model = pipeline("question-answering", "timpal0l/mdeberta-v3-base-squad2")
 name = "What is the name of the hackathon?"
 location = "Where is the hackathon?"
-date = "On which date does the hackathon take place?"
+date = "When is the hackathon?"
 topics = "What topics does the hackathon center around?"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    text = re.sub(r"\n{2,}", "\n", soup.text.strip())
+    soup = BeautifulSoup(response.content, "html.parser")
+    text = re.sub(r"\s+", " ", soup.text.strip())
     with open("hack.html", "w") as f:
         f.write(str(soup.html))
     with open("hack.txt", "w") as f:
         f.write(soup.text)
-    #qa_model(question = location, context = text)
-    #print(ask_llm(url, text))
+    with open("hack.clean.txt", "w") as f:
+        f.write(text)
+
+    description = soup.find("meta", attrs={"name": "description"})
+    if description and description.get("content"):
+        print(description["content"])
+
 else:
-    print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
+    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
